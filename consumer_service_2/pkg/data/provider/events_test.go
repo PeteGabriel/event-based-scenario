7b3c28bbbc6f334849c4973024b3a6a53b64b42f
PeteGabriel <pedrogabriel@protonmail.ch>
2021-10-08 00:34:43 +0200

    Add unit test file.

---
+++ consumer_service_2/pkg/data/provider/events_test.go
@@ -0,0 +1,30 @@
+package provider
+
+import (
+	"petegabriel/consumer_svc_2/pkg/config"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	s := config.New("../../../test.env")
+
+	if _, err := New(s); err != nil {
+		t.Errorf("error creating consumer: %s", err)
+		t.FailNow()
+	}
+}
+
+func TestMessenger_PublishMessage(t *testing.T) {
+	s := config.New("../../../test.env")
+
+	ec, err := New(s)
+	if err != nil {
+		t.FailNow()
+	}
+
+
+	if ch, err := ec.GetEventChannel(); err != nil || ch == nil{
+		t.Errorf("error consuming event from queue: %s", err)
+		t.FailNow()
+	}
+}
